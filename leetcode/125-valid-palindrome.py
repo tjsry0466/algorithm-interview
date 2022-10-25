5008bc93d61155856f37a2949a9dd9e223607818
seonkyo <tjs0466@naver.com>
2022-10-25 22:28:17 +0900

    125 주석 작성

--- leetcode/125-valid-palindrome.py
+++ leetcode/125-valid-palindrome.py
@@ -1,9 +1,11 @@
-# pop()으로 구현
 import collections
 import re
 from typing import Deque
 
 
+# pop()으로 구현
+# 맨 왼쪽과 맨 뒤 값을 하나씩 빼면서 같은 값인지 확인
+# 틀리다면 팰린더룸이 아님
 def isPalindrome(s: str) -> bool:
     strs = []
     for char in s:
@@ -14,10 +16,14 @@ def isPalindrome(s: str) -> bool:
         if strs.pop(0) != strs.pop():
             return False
     return True
+
+
 s = "A man, a plan, a canal: Panama"
 print(isPalindrome(s))
 
+
 # 데크로 구현
+# 위와 로직은 동일 데크를 사용하게되면 popLeft의 시간복잡도가 O(1)이기 때문에 더 빠르게 구현할 수 있음
 def isPalindrome1(s: str) -> bool:
     # 자료형 데크로 선언
     strs: Deque = collections.deque()
@@ -31,14 +37,19 @@ def isPalindrome1(s: str) -> bool:
             return False
 
     return True
+
+
 print(isPalindrome1(s))
 
+
+# 숫자와 문자만 골라낸 뒤에 뒤집은 것과 동일한지 체크
 # 슬라이싱 사용
 def isPalindrome2(s: str) -> bool:
     s = s.lower()
     # 정규식으로 불필요한 문자 필터링
     s = re.sub('[^a-z0-9]', '', s)
 
-    return s == s[::-1] # 슬라이싱
-print(isPalindrome2(s))
+    return s == s[::-1]  # 슬라이싱
 
+
+print(isPalindrome2(s))
